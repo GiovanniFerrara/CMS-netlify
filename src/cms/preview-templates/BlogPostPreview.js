aa971e678a26bec331fd6e66e3e39d13a0175d0a
GiovanniFerrara <gianmarco.ferrara@gmail.com>
2018-12-31 01:03:21 +0100

    starts working

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import BlogPostTemplate  from '../../templates/PostTemplate'
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import BlogPostTemplate  from '../../templates/PostTemplate'
 
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    data= {{
-      post: {
-        html: entry.getIn(['data','body'])
-      }
-    }}
-    // content={widgetFor('body')}
-    // description={entry.getIn(['data', 'description'])}
-    // tags={entry.getIn(['data', 'tags'])}
-    // title={entry.getIn(['data', 'title'])}
-  />
-)
+// const BlogPostPreview = ({ entry, widgetFor }) => (
+//   <BlogPostTemplate
+//     data= {{
+//       post: {
+//         html: entry.getIn(['data','body'])
+//       }
+//     }}
+//     // content={widgetFor('body')}
+//     // description={entry.getIn(['data', 'description'])}
+//     // tags={entry.getIn(['data', 'tags'])}
+//     // title={entry.getIn(['data', 'title'])}
+//   />
+// )
 
-BlogPostPreview.propTypes = {
-  entry: PropTypes.shape({
-    getIn: PropTypes.func,
-  }),
-  widgetFor: PropTypes.func,
-}
+// BlogPostPreview.propTypes = {
+//   entry: PropTypes.shape({
+//     getIn: PropTypes.func,
+//   }),
+//   widgetFor: PropTypes.func,
+// }
 
-export default BlogPostPreview
+// export default BlogPostPreview
