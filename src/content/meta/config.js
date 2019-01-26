7f9f2804743b1836e79b6e8bc3fe8c5e22ee0ddc
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2019-01-26 21:56:01 +0100

    custom stuff

--- src/content/meta/config.js
+++ src/content/meta/config.js
@@ -12,8 +12,8 @@ module.exports = {
   authorTwitterAccount: "greglobinski",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "Alessandro Ferrara",
+  headerSubTitle: "Scrittore di storie di sport",
 
   /* manifest.json */
   manifestName: "HeroBlog - a GatsbyJS starter",
